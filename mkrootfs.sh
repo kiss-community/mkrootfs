66bc5820536b8746c7bd60dc06a8ce346fab9aca
Cem Keylan <cem@ckyln.com>
2019-12-18 10:40:03 +0300

    remove unnecessary kiss i

--- mkrootfs.sh
+++ mkrootfs.sh
@@ -82,10 +82,6 @@ msg "Starting build from the PKGS variable"
 # shellcheck disable=SC2086
 kiss b $PKGS
 
-msg "Package build complete, starting package installation"
-# shellcheck disable=SC2086
-kiss i $PKGS
-
 # You can check out about post-installation 
 # from the configuration file
 msg "Installation Complete, starting custombuild procedure if there is one"
